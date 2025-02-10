90d6ed2a6e51ee2dd3c447931568fba820fb699c
KartikKulshreshtha <76987281+KartikKulshreshtha@users.noreply.github.com>
2025-02-10 12:28:39 +0530

    Added MongoDB url

--- config/db.js
+++ config/db.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/taskManagementsys"; // Local fallback
+    const MONGO_URI = process.env.MONGODB_URL; // Local fallback
     await mongoose.connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log(`MongoDB Connected: ${MONGO_URI}`);
+    console.log(`MongoDB Connected`);
   } catch (error) {
     console.error("DB Connection Error:", error);
     process.exit(1);
